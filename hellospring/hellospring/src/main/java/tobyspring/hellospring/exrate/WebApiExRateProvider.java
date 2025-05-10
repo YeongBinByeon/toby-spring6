ba8f3d9821c56ee198ee4db2a3231f94239a4081
yeongbin <yeongbin6483@gmail.com>
2025-05-11 00:21:23 +0900

    24. 의존성 역전 원칙(DIP)

--- hellospring/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ hellospring/hellospring/src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,7 +1,8 @@
-package tobyspring.hellospring;
+package tobyspring.hellospring.exrate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
+import tobyspring.hellospring.payment.ExRateProvider;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,7 +13,7 @@ import java.net.URL;
 import java.util.stream.Collectors;
 
 @Component
-public class WebApiExRateProvider implements ExRateProvider{
+public class WebApiExRateProvider implements ExRateProvider {
 
     @Override
     public BigDecimal getExRate(String currency) throws IOException {
